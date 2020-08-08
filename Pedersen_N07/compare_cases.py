e326f211fdf1cdddd3143c6aa7466885d51453cd
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2020-08-08 13:11:50 -0600

    Add N07 Amrwind-ksgs data

---
+++ Pedersen_N07/compare_cases.py
@@ -0,0 +1,98 @@
+# coding: utf-8
+import load_data, sys
+sys.path.insert(1, '../utilities')
+import windspectra
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+l_cases = {
+    'amrwind-ksgs': load_data.AMRWindStats('AmrWindKsgs'),
+    'naluwind-smag': load_data.NaluWindStats('NaluWindRun01'),
+    'naluwind-ksgs': load_data.NaluWindStats('NaluWindRun02'),
+    'Pederson:2014': load_data.PedersonData('pedersen2014_data')
+    }
+
+with PdfPages('nalu_amr_comparison_n07.pdf') as pfpgs:
+    plt.style.use('singleColumn')
+    fig = plt.figure()
+    for l,c in l_cases.items():
+        plt.plot(c.hvelmag, c.z, label=l)
+    plt.ylim()
+    plt.xlabel(r'$< | \vec{u}_{\textrm{horiz}} | >$')
+    plt.ylabel('z')
+    plt.legend(loc=0)
+    plt.ylim(0,750)
+    plt.grid()
+    plt.tight_layout()
+    pfpgs.savefig()
+    plt.close(fig)
+
+    fig = plt.figure()
+    for l,c in l_cases.items():
+        plt.plot(c.T, c.z, label=l)
+    plt.xlabel(r'$<T>$')
+    plt.ylabel('z')
+    plt.legend(loc=0)
+    plt.ylim(0,1000)
+    plt.grid()
+    plt.tight_layout()
+    pfpgs.savefig()
+    plt.close(fig)
+
+    plt.style.use('singleColumn.square')
+    deltaX=3000.0/288         # Grid spacing
+    for i, z in enumerate(l_cases['naluwind-ksgs'].ps_data['z']):
+        f, suu, svv, sww = l_cases['naluwind-ksgs'].point_spectra(z)
+        utau = l_cases['naluwind-ksgs'].istats['ustar']
+        hvelmag = l_cases['naluwind-ksgs'].interp_hvelmag(z)
+        f_a, suu_a, svv_a, sww_a = l_cases['amrwind-ksgs'].point_spectra(z)
+        utau_a = float(l_cases['amrwind-ksgs'].istats['ustar'])
+        fmax = 0.6*hvelmag/(8*deltaX)
+        
+        fig,ax = plt.subplots()
+        lineu, = ax.loglog(f, windspectra.getKaimal(f, z, hvelmag ),label='Kaimal - u')
+        ax.loglog(f, f * suu/(utau * utau) , '--', label='naluwind-ksgs - u')
+        ax.loglog(f_a, f_a * suu_a/(utau_a * utau_a) , '-.', label='amr-wind-ksgs - u')
+        plt.vlines(fmax, 5e-4, 20,lw=3, linestyle='-.')
+        plt.xlim(1e-4,2)
+        plt.ylim(1e-3,15)
+        plt.grid()
+        plt.xlabel('Frequency $f$ [Hz]')
+        plt.ylabel('$f\cdot S_{u}/u_{\\tau}^2$ [-]')
+        plt.title('Avg spectra [Neutral N07, z={:.1f}m]'.format(z))
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+        pfpgs.savefig()
+        plt.close(fig)
+        
+        fig,ax = plt.subplots()
+        linev, = ax.loglog(f, windspectra.getKaimal(f, z, hvelmag, params=windspectra.vKaimalconst),label='Kaimal - v')
+        ax.loglog(f, f * svv/(utau * utau) , '--', label='naluwind-ksgs - v')
+        ax.loglog(f_a, f_a * svv_a/(utau_a * utau_a) , '-.', label='amr-wind-ksgs - v')
+        plt.vlines(fmax, 5e-4, 20,lw=3, linestyle='-.')
+        plt.xlim(1e-4,2)
+        plt.ylim(1e-3,15)
+        plt.grid()
+        plt.xlabel('Frequency $f$ [Hz]')
+        plt.ylabel('$f\cdot S_{v}/u_{\\tau}^2$ [-]')
+        plt.title('Avg spectra [Neutral N07, z={:.1f}m]'.format(z))
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+        pfpgs.savefig()
+        plt.close(fig)
+        
+        fig,ax = plt.subplots()
+        linew, = ax.loglog(f, windspectra.getKaimal(f, z, hvelmag, params=windspectra.wKaimalconst),label='Kaimal - w')
+        ax.loglog(f, f * sww/(utau * utau) , '--', label='naluwind-ksgs - w')
+        ax.loglog(f_a, f_a * sww_a/(utau_a * utau_a) , '-.', label='amr-wind-ksgs - w')
+        plt.vlines(fmax, 5e-4, 20,lw=3, linestyle='-.')
+        plt.xlim(1e-4,2)
+        plt.ylim(1e-3,15)
+        plt.grid()
+        plt.xlabel('Frequency $f$ [Hz]')
+        plt.ylabel('$f\cdot S_{w}/u_{\\tau}^2$ [-]')
+        plt.title('Avg spectra [Neutral N07, z={:.1f}m]'.format(z))
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+        pfpgs.savefig()
+        plt.close(fig)
---
+++ Pedersen_N07/load_data.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pandas as pd
+import glob, yaml, pathlib
+from netCDF4 import Dataset
+from scipy.interpolate import interp1d
+
+class ABLStats:
+  """Generic class that contains abl statistics"""
+  def __init__(self):
+    self.z = []
+    self.u = []
+    self.v = []
+    self.w = []
+    self.hvelmag = []
+    self.T = []
+    self.istats = {
+      'ustar': 0.0,
+      'wstar': 0.0,
+      'L': 0.0,
+      'Q': 0.0,
+      'Tsurf': 0.0,
+      'zi': 0.0
+    }
+    self.ps_data = {
+      'z': [],
+      'f': [],
+      'suu': [],
+      'svv': [],
+      'sww': []
+    }
+
+  def interp_hvelmag(self, z):
+    return np.interp(z, self.z, self.hvelmag)
+
+  def point_spectra(self, z):
+    suu = interp1d(self.ps_data['z'], self.ps_data['suu'],axis=0)(z)
+    svv = interp1d(self.ps_data['z'], self.ps_data['svv'],axis=0)(z)
+    sww = interp1d(self.ps_data['z'], self.ps_data['sww'],axis=0)(z)
+    return self.ps_data['f'], suu, svv, sww
+  
+class NaluWindStats(ABLStats):
+  """ABLStats class specific to NaluWind output"""
+  def __init__(self,dir_name):
+    vel_file = glob.glob(dir_name+'/*velocity.dat')[0]
+    a = pd.read_csv(vel_file, sep='\s+', skiprows=1, names=['z','u','v','w','u_mag'] )
+    self.z = a['z'].values
+    self.u = a['u'].values
+    self.v = a['v'].values
+    self.w = a['w'].values
+    self.hvelmag = a['u_mag'].values
+    temp_file = glob.glob(dir_name+'/*temperature.dat')[0]
+    a = pd.read_csv(temp_file, sep='\s+', skiprows=1, names=['z','T'] )
+    self.T = a['T'].values
+
+    spectra_files = glob.glob(dir_name+'/*spectra*dat')
+    self.ps_data = {
+      'z': np.array(sorted([ float(pathlib.Path(i).with_suffix('').name.split('_')[-1][1:]) for i in spectra_files])),
+      'f': pd.read_csv(spectra_files[0],sep='\s+',skiprows=1,header=None,usecols=[0]).values[:,0],
+      'suu': np.array([pd.read_csv(i,sep='\s+',skiprows=1,header=None,usecols=[1]).values[:,0] for i in spectra_files ] ),
+      'svv': np.array([pd.read_csv(i,sep='\s+',skiprows=1,header=None,usecols=[2]).values[:,0] for i in spectra_files ] ),
+      'sww': np.array([pd.read_csv(i,sep='\s+',skiprows=1,header=None,usecols=[3]).values[:,0] for i in spectra_files ] )
+    }
+    self.istats = {
+      'ustar': 0.41,
+      'wstar': 0.0,
+      'L': -1111.1,
+      'Q': 0.005,
+      'Tsurf': 0.0,
+      'zi': 757.0
+    }
+
+class AMRWindStats(ABLStats):
+  """ABLStats class specific to AMRWind output"""
+  def __init__(self,dir_name):
+    a = pd.read_csv(dir_name+'/line_average_data.csv')
+    self.z = a['z'].values
+    self.u = a['<u>'].values
+    self.v = a['<v>'].values
+    self.w = a['<w>'].values
+    self.hvelmag = a['<hvelmag>'].values
+    self.T = a['<temperature0>'].values
+    self.vel_var = {
+      "<u'u'>" : a["<u'u'>"],
+      "<u'v'>" : a["<u'v'>"],
+      "<u'w'>" : a["<u'w'>"],
+      "<v'v'>" : a["<v'v'>"],
+      "<v'w'>" : a["<v'w'>"],
+      "<w'w'>" : a["<w'w'>"],
+      "<w'w'w'>": a["<w'w'w'>"]
+    }
+    self.istats = yaml.load(open(dir_name+'/istats.yaml'),Loader=yaml.BaseLoader)
+
+    with Dataset(dir_name+'/avg_spectra.nc') as d:
+      self.ps_data = {
+        'z': d.variables['z'][:],
+        'f': d.variables['f'][:],
+        'suu': d.variables['suu'][:,:],
+        'svv': d.variables['svv'][:,:],
+        'sww': d.variables['sww'][:,:]
+      }
+    
+    
+class PedersonData(ABLStats):
+  """ABLStats class specific to Pederson2014 data"""
+  def __init__(self, dir_name):
+    self.istats = yaml.load(open(dir_name+'/istats.yaml'),Loader=yaml.BaseLoader)
+    a = pd.read_csv(dir_name+'/umag.csv',skiprows=1,names=['hvelmag','z'],dtype=float)
+    self.z = a['z'].values * float(self.istats['zi'])
+    self.hvelmag = a['hvelmag'].values
+    a = pd.read_csv(dir_name+'/temperature.csv',skiprows=1,names=['T','z'],dtype=float)
+    self.T = np.interp(self.z, a['z'].values, a['T'].values)
+
+        
